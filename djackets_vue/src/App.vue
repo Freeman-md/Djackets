ff2795b8ce10f3716ed6c48ab495a9f5b4ddd643
Freeman-md <freemanmadudili@gmail.com>
2021-06-16 21:41:02 +0100

    Setup Base Template For Vue

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -6,14 +6,14 @@
         <router-link to="/" class="navbar-item">
         <strong>Djackets</strong></router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" :class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
             <router-link to="/summer" class="navbar-item">Summer</router-link>
             <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -44,5 +44,16 @@
 </template>
 
 <style lang="scss">
-@import '../node_modules/bulma'
+@import '../node_modules/bulma';
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  },  
+}
+
+</script>
