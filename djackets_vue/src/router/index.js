ae9fdfd42d2a964465026c53c7e065d19dcca8ac
Freeman-md <freemanmadudili@gmail.com>
2021-06-18 09:42:00 +0100

    Success and Checkout Page Implemented

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -9,6 +9,8 @@ import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
 import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
+import Success from '../views/Success.vue'
 
 const routes = [{
         path: '/',
@@ -58,6 +60,22 @@ const routes = [{
         path: '/my-account',
         name: 'MyAccount',
         component: MyAccount,
+        meta: {
+            requireLogin: true
+        }
+    },
+    {
+        path: '/cart/checkout',
+        name: 'Checkout',
+        component: Checkout,
+        meta: {
+            requireLogin: true
+        }
+    },
+    {
+        path: '/cart/success',
+        name: 'Success',
+        component: Success,
         meta: {
             requireLogin: true
         }
